1aa9b72954ba95eda4325c11f98a224952707941
bhup-m <117510059+bhup-m@users.noreply.github.com>
2023-03-03 20:24:56 +0530

    Delete backend directory

--- backend/Models/userModel.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-const userInfoSchema = new mongoose.Schema({
-    name:String,
-    password:Number
-})
-
-const User = mongoose.model("User",userInfoSchema)
-
-exports.User = User;
